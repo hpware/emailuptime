779ad291bac439e9f56bd96aa4bf0930a725bc3e
Howard <hw@yuanhau.com>
2025-05-17 14:46:50 +0800

    Add empy files (and change to VS Code)

--- app/main.py
+++ app/main.py
@@ -1,11 +1,36 @@
+# Imports
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import dotenv
 import os
 import uuid
+from flask import Flask
 
+# Init
+app = Flask(__name__)
 dotenv.load_dotenv()
 
+# Flask
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-def main():
-    print("Hello flask!")
+@app.route("/gmail")
+def gmailStatus():
+    return "sys"
+
+@app.route("/outlook")
+def outlookStatus():
+    return "sys"
+
+@app.route("/yahoo")
+def yahooStatus():
+    return "sys"
+
+@app.route("/icloud")
+def icloudStatus():
+    return "sys"
+
+@app.route("/fastmail")
+def fastmailStatus():
+    return "sys"
